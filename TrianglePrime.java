75ddc8ca5965e8cc69686657e3a4d875cd2570aa
Raju <88134926+dev-rajukumar@users.noreply.github.com>
2021-07-29 15:58:14 +0530

    Pyramid of Prime Number
    Prime numbers is Pyramid Shape in java

--- TrianglePrime.java
+++ TrianglePrime.java
@@ -15,18 +15,16 @@ class TrianglePrime
 				}
 			}
 				if(temp==0)
-				{
-				for(int i=1; i<=4; i++)
-	      {
-               for(int j=1; j<=i; j++)
-                 {
-                  System.out.print(no);
-                 }
-                System.out.println();
-              }   
-
-		}
-
-               }
+		          {
+				for(int j=1; j<=no; j++)
+                                {
+                                 System.out.print(" ");
+		                 }
+				for(int k=11; k>=no; k--){
+                                 System.out.print(no + " ");
+				}
+                                 System.out.println();
+		           }
+                   }
         }
 }
